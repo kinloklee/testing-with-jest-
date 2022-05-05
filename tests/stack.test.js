cd68bb0d59510c7a71ea746452d49198a0c95b4c
kinloklee <your_github_emailcd>
2022-05-05 14:44:04 +0200

    Medvetet fel

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,13 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+test('peek on stack with two or more elements returns the top element', () => {
+    stack.push(5000);
+    stack.push(9000);
+    stack.push(10000);
+    expect(stack.peek()).toBeDefined();
+    stack.pop()
+    expect(stack.peek()).toBe(10000);
+    
+});    
