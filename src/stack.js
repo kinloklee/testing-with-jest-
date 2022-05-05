13bf5cfff955ee837e8a512877fe12e8ebb1bb7d
kinloklee <your_github_emailcd>
2022-05-05 13:43:05 +0200

    JÄndringar

--- src/stack.js
+++ src/stack.js
@@ -14,5 +14,5 @@ exports.pop = function () {
 
 // Returnerar det översta elementet i stacken
 exports.peek = function () {
-    return _.last(stack);
+    return stack[0]; // Det här är medvetet felaktigt
 }
